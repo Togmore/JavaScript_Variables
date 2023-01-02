0f5b390bdc2a055e08c0b5bfd29ef433b113a0b6
Thomas Rossetti <thomas.rossetti@gmail.com>
2023-01-02 12:01:47 -0700

    Finished the exercise and added a comment on console.log

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = "Thomas";
+let lastName = "Rossetti";
+var age = 43;
 
-// Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1979;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let PageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log("This is my first comment on the console.log . Will it appear in the place I think it will appear in??")
